60e6e4761522533af5915076dc82c3d13ae592c7
Richard Musiol <mail@richard-musiol.de>
2015-09-28 01:40:57 +0200

    bit more interesting example

--- example/example.go
+++ example/example.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"github.com/neelance/dom"
+	"github.com/neelance/dom/elem"
 	_ "github.com/neelance/dom/example/components/impl"
 	"github.com/neelance/dom/example/components/spec"
 )
 
 func main() {
 	dom.RenderAsBody(
-		&spec.Greeter{
-			Greeting: "Hello",
-		},
+		elem.Div(
+			dom.Text("This is a very simple component:"),
+			&spec.Greeter{
+				Greeting: "Hello",
+			},
+		),
 	)
 }
