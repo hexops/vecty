2b6f59b0cfc44941f4a23c9875f5c56b22cdbb6e
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2016-02-21 00:44:27 -0700

    move all Markup into markup.go (easier to digest)

--- dom.go
+++ dom.go
@@ -1,15 +1,6 @@
 package vecty
 
-import (
-	"fmt"
-	"strings"
-
-	"github.com/gopherjs/gopherjs/js"
-)
-
-type Markup interface {
-	Apply(element *Element)
-}
+import "github.com/gopherjs/gopherjs/js"
 
 type Component interface {
 	Markup
@@ -149,72 +140,6 @@ func (e *Element) Node() *js.Object {
 	return e.node
 }
 
-type Property struct {
-	Name  string
-	Value interface{}
-}
-
-func (p *Property) Apply(element *Element) {
-	if element.Properties == nil {
-		element.Properties = make(map[string]interface{})
-	}
-	if _, ok := element.Properties[p.Name]; ok {
-		panic(fmt.Sprintf("duplicate property: %s", p.Name))
-	}
-	element.Properties[p.Name] = p.Value
-}
-
-type ClassMap map[string]bool
-
-func (m ClassMap) Apply(element *Element) {
-	var classes []string
-	for name, active := range m {
-		if active {
-			classes = append(classes, name)
-		}
-	}
-	p := Property{
-		Name:  "className",
-		Value: strings.Join(classes, " "),
-	}
-	p.Apply(element)
-}
-
-type Style struct {
-	Name  string
-	Value interface{}
-}
-
-func (s *Style) Apply(element *Element) {
-	if element.Style == nil {
-		element.Style = make(map[string]interface{})
-	}
-	if _, ok := element.Style[s.Name]; ok {
-		panic(fmt.Sprintf("duplicate style: %s", s.Name))
-	}
-	element.Style[s.Name] = s.Value
-}
-
-type EventListener struct {
-	Name               string
-	Listener           func(*Event)
-	CallPreventDefault bool
-	wrapper            func(jsEvent *js.Object)
-}
-
-func (l *EventListener) PreventDefault() *EventListener {
-	l.CallPreventDefault = true
-	return l
-}
-
-func (l *EventListener) Apply(element *Element) {
-	element.EventListeners = append(element.EventListeners, l)
-}
-
-type Event struct {
-	Target *js.Object
-}
-
 // SetTitle sets the title of the document.
 func SetTitle(title string) {
 	js.Global.Get("document").Set("title", title)
@@ -226,23 +151,6 @@ func AddStylesheet(url string) {
 	link.Set("rel", "stylesheet")
 	link.Set("href", url)
 	js.Global.Get("document").Get("head").Call("appendChild", link)
-}
-
-type List []Markup
-
-func (g List) Apply(element *Element) {
-	for _, m := range g {
-		if m != nil {
-			m.Apply(element)
-		}
-	}
-}
-
-func If(cond bool, markup ...Markup) Markup {
-	if cond {
-		return List(markup)
-	}
-	return nil
 }
 
 type Composite struct {
---
+++ markup.go
@@ -0,0 +1,95 @@
+package vecty
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/gopherjs/gopherjs/js"
+)
+
+type Markup interface {
+	Apply(element *Element)
+}
+
+type Property struct {
+	Name  string
+	Value interface{}
+}
+
+func (p *Property) Apply(element *Element) {
+	if element.Properties == nil {
+		element.Properties = make(map[string]interface{})
+	}
+	if _, ok := element.Properties[p.Name]; ok {
+		panic(fmt.Sprintf("duplicate property: %s", p.Name))
+	}
+	element.Properties[p.Name] = p.Value
+}
+
+type ClassMap map[string]bool
+
+func (m ClassMap) Apply(element *Element) {
+	var classes []string
+	for name, active := range m {
+		if active {
+			classes = append(classes, name)
+		}
+	}
+	p := Property{
+		Name:  "className",
+		Value: strings.Join(classes, " "),
+	}
+	p.Apply(element)
+}
+
+type Style struct {
+	Name  string
+	Value interface{}
+}
+
+func (s *Style) Apply(element *Element) {
+	if element.Style == nil {
+		element.Style = make(map[string]interface{})
+	}
+	if _, ok := element.Style[s.Name]; ok {
+		panic(fmt.Sprintf("duplicate style: %s", s.Name))
+	}
+	element.Style[s.Name] = s.Value
+}
+
+type EventListener struct {
+	Name               string
+	Listener           func(*Event)
+	CallPreventDefault bool
+	wrapper            func(jsEvent *js.Object)
+}
+
+func (l *EventListener) PreventDefault() *EventListener {
+	l.CallPreventDefault = true
+	return l
+}
+
+func (l *EventListener) Apply(element *Element) {
+	element.EventListeners = append(element.EventListeners, l)
+}
+
+type Event struct {
+	Target *js.Object
+}
+
+type List []Markup
+
+func (g List) Apply(element *Element) {
+	for _, m := range g {
+		if m != nil {
+			m.Apply(element)
+		}
+	}
+}
+
+func If(cond bool, markup ...Markup) Markup {
+	if cond {
+		return List(markup)
+	}
+	return nil
+}
