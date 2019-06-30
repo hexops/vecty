9e4a6674ef7b3d7e08b43945675e8df9c1a736a7
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2019-06-29 19:48:01 -0700

    require GopherJS version 1.12+ for syscall/js

--- require_go_1_12.go
+++ require_go_1_12.go
@@ -1,16 +1,15 @@
-// +build wasm
 // +build !go1.12
 
 package vecty
 
 func init() {
-	VECTY_WEB_ASSEMBLY_REQUIRES_GO_1_12_PLUS = "Vecty WebAssembly requires Go 1.12+"
+	VECTY_REQUIRES_GO_1_12_PLUS = "Vecty requires Go 1.12+"
 }
 
 // Stubs to ensure typechecking passes except one error:
 //
 // 	# github.com/gopherjs/vecty
-// 	../../require_go_1_12.go:7:2: undefined: VECTY_WEB_ASSEMBLY_REQUIRES_GO_1_12_PLUS
+// 	../../require_go_1_12.go:7:2: undefined: VECTY_REQUIRES_GO_1_12_PLUS
 //
 
 var (
