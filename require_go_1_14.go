2c970b5a05a800216c0d139f31193c9fda80861c
Stephen Gutekanst <stephen@hexops.com>
2020-08-15 19:40:29 -0700

    properly denote Go 1.14+ requirement

---
+++ require_go_1_14.go
@@ -0,0 +1,13 @@
+// +build !go1.14
+
+package vecty
+
+// Typechecking will pass except one error:
+//
+// 	# github.com/gopherjs/vecty
+// 	../../require_go_1_14.go:7:2: undefined: VECTY_REQUIRES_GO_1_14_PLUS
+//
+
+func init() {
+	VECTY_REQUIRES_GO_1_14_PLUS = "Vecty requires Go 1.14+"
+}
